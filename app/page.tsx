f5519fa2a7c2dc80acadeb364526647cab2f6a14
Heriz Shrestha <19641023+hstha@users.noreply.github.com>
2025-03-15 23:52:54 +0545

    Add Trunk configuration and linting setup - Introduced .gitignore to exclude unnecessary files - Added trunk.yaml for Trunk CLI configuration with plugins and runtimes - Configured markdownlint and yamllint with specific rules - Included SVGO configuration for SVG optimization - Updated page.tsx to enhance layout structure

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <p className="text-red-500">Hello Notion Clone</p>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <p className="text-red-500">Hello Notion Clone</p>
+    </div>
   );
 }
